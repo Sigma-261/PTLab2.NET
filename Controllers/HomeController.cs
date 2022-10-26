d8b7752d42ecce3ab315f8df610ef8fa40d577c3
Mihail <darkserj18@gmail.com>
2022-10-26 17:34:12 +0300

    Добавьте файлы проекта.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PTLab2_Final.Models;
+using System.Diagnostics;
+
+namespace PTLab2_Final.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        ApplicationContext db;
+        public HomeController(ILogger<HomeController> logger, ApplicationContext context)
+        {
+            _logger = logger;
+            db = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await db.Users.ToListAsync());
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(User user)
+        {
+            db.Users.Add(user);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Electronic.cs
@@ -0,0 +1,12 @@
+﻿namespace PTLab2_Final.Models
+{
+    public class Electronic
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; } // имя товара
+        public string? Category { get; set; } // категория товара
+        public float? Price { get; set; } // цена товара
+        public int ForSale { get; set; } // кол-во для продажи
+        public int Sold { get; set; } // кол-во проданного товара
+    }
+}
